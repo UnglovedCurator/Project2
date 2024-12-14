b543ab98a8544d08b127894b49693edf85e4e830
UnglovedCurator <stellarviking@protonmail.com>
2024-12-14 23:05:23 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', function() {
-        const effectClass = {
-            'btn1': 'pulse',
-            'btn2': 'shake',
-            'btn3': 'rotate',
-            'btn4': 'rainbow'
-        }[this.id];
+const interactiveButton = document.querySelector('.interactive-button');
+const dynamicText = document.getElementById('dynamic-text');
+const changeTextButton = document.getElementById('change-text-button');
 
-        this.classList.add(effectClass);
-        
-        setTimeout(() => {
-            this.classList.remove(effectClass);
-        }, 1000);
-    });
+interactiveButton.addEventListener('click', () => {
+    alert('Button clicked!');
+});
+
+const phrases = [
+    "Welcome to our website!",
+    "Explore the latest in web design.",
+    "Interactive elements make it fun!",
+    "Thanks for visiting!"
+];
+
+let currentPhraseIndex = 0;
+
+changeTextButton.addEventListener('click', () => {
+    currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+    dynamicText.textContent = phrases[currentPhraseIndex];
 });
