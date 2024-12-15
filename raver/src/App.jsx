85ab24ee3a588308e41d26344e8e11386a016235
UnglovedCurator <stellarviking@protonmail.com>
2024-12-15 13:05:57 +0100

    Initial commit

--- raver/src/App.jsx
+++ raver/src/App.jsx
@@ -1,14 +1,27 @@
+import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import AnimatedBackground from './components/AnimatedBackground';
+import UpcomingEvents from './components/UpcomingEvents';
+import FeaturedArtists from './components/FeaturedArtists';
+
+const AppContainer = styled.div`
+  background-color: var(--color-primary);
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <div>
+    <AppContainer>
       <GlobalStyles />
       <Navbar />
-      <Hero />
-    </div>
+      <Hero>
+        <AnimatedBackground /> {/* Add this inside Hero */}
+      </Hero>
+      <UpcomingEvents />
+      <FeaturedArtists />
+    </AppContainer>
   );
 }
 
